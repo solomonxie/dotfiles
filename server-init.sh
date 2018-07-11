ac80b38288e676537d900bd8d57e13d48e2d4242
Solomon Xie <solomonxiewise@gmail.com>
2018-07-11 11:58:57 +0800

    Update from Solomon-mba.local

--- server-init.sh
+++ server-init.sh
@@ -8,3 +8,23 @@ yes | sudo apt-get update
 yes | sudo apt-get upgrade
 yes | sudo apt-get install htop
 
+
+# Setup Python3
+
+
+# Setup Shadowsocks
+
+
+
+# Setup ZSH
+
+
+# Setup Vim
+
+
+
+
+
+# Install Useful Programs
+yes | sudo pip install -U youtube-dl you-get
+yes | sudo pip install --user pipenv

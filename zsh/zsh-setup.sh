b46b799c0f1fc8844399ea3d5121e4640024dd32
Solomon Xie <solomonxiewise@gmail.com>
2018-07-11 14:11:24 +0800

    Update from Solomon-mba.local

--- zsh/zsh-setup.sh
+++ zsh/zsh-setup.sh
@@ -1,14 +1,18 @@
 # Owner: Solomon Xie
 # Email: solomonxiewise@gmail.com
-# Enviroment: MacOS Sierra
+# Enviroment: Ubuntu
 #
 # =======SETING UP ZSH =======
+echo "========== (START SETTING UP ZSH) ==========="
 yes | sudo apt-get install zsh
+echo "========== (INSTALLING OH-MY-ZSH) ==========="
 sudo curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 # install plugins
+echo "========== (INSTALLING PLUGINS FOR ZSH) ==========="
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 sudo git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 yes | sudo pip install --user powerline-status
 # Setup configs
+echo "========== (OVERWRITE ZSHRC) ==========="
 sudo wget https://github.com/solomonxie/cdn/raw/master/zsh/zshrc -O ~/.zshrc

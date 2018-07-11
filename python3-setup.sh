f3b5394a6337466d3e10542d252664ae220a6dc1
Solomon Xie <solomonxiewise@gmail.com>
2018-07-11 11:18:36 +0800

    server init scripts

---
+++ python3-setup.sh
@@ -0,0 +1,7 @@
+
+# ======= Upgrade & Setup Python3 =======
+yes | sudo apt-get install python python3
+sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+yes | sudo python get-pip.py
+yes | sudo pip install --upgrade pip
+yes | sudo pip install virtualenv pipenv
---
+++ tmux-setup.sh
@@ -0,0 +1,5 @@
+
+# =======SETING UP TMUX =======
+sudo apt-get install tmux
+sudo git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+touch ~/.tmux.conf

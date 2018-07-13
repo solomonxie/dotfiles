8076896939279791e84b16bf792258e7caa21f25
Solomon Xie <solomonxiewise@gmail.com>
2018-07-13 19:34:28 +0800

    add OwnCloud installation & setup script

--- server/python3-setup.sh
+++ server/python3-setup.sh
@@ -1,4 +1,8 @@
-
+# Owner: Solomon Xie
+# Email: solomonxiewise@gmail.com
+# Preload:
+# Commands:
+#
 # ======= Upgrade & Setup Python3 =======
 yes | sudo apt-get install python python3
 sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
--- server/tmux-setup.sh
+++ server/tmux-setup.sh
@@ -1,3 +1,8 @@
+# Owner: Solomon Xie
+# Email: solomonxiewise@gmail.com
+# Preload:
+# Commands:
+#
 # =======SETING UP TMUX =======
 sudo apt-get install tmux
 sudo git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

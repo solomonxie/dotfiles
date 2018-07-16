705ad4e3c73d0e66e0d34947c6c36d8dfda7133b
Solomon Xie <solomonxiewise@gmail.com>
2018-07-16 13:47:23 +0800

    samba

--- Rpi/samba.sh
+++ Rpi/samba.sh
@@ -10,29 +10,24 @@ sudo apt-get update
 sudo apt-get install samba samba-common-bin
 
 
-
 echo "============== (Configure the sharing folder with Samba) ======================"
-sudo mkdir -m 1777 /home/pi/share
-sudo cat> /etc/samba/smb.conf <<EOF
-[share]
-Comment = Pi shared folder
-Path = /home/pi/share
-Browseable = yes
-Writeable = Yes
-only guest = no
-create mask = 0777
-directory mask = 0777
-Public = yes
-Guest ok = yes
+sudo cat>> /etc/samba/smb.conf <<EOF
+[NAS]
+   comment = NAS External drive
+   path = /media/pi
+   public = Yes
+   browseable = Yes
+   writeable = Yes
+   valid users=pi
 EOF
 
 echo "============== (Add local user & Samba user) ======================"
-sudo groupadd samba -g 6000
-sudo useradd samba1 -u 6000 -g 6000 -s /sbin/nologin -d /dev/null
+#sudo groupadd samba -g 6000
+#sudo useradd samba1 -u 6000 -g 6000 -s /sbin/nologin -d /dev/null
 sudo smbpasswd -a samba1
 
 echo "============== (Restarting Samba server) ======================"
 sudo /etc/init.d/samba restart
 
-echo "============== (Installment of Samba server finished.) ======================"
+echo "============== (Samba server is ready to go.) ======================"
 

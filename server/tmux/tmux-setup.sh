d40b655bd6145b3480e00e7efaa214b745e33d3e
Solomon Xie <solomonxiewise@gmail.com>
2018-07-18 23:55:09 +0800

    updated tmux

--- server/tmux/tmux-setup.sh
+++ server/tmux/tmux-setup.sh
@@ -6,4 +6,4 @@
 # =======SETING UP TMUX =======
 sudo apt-get install tmux
 sudo git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sudo wget https://github.com/solomonxie/cdn/raw/master/server/tmux.conf -O ~/.tmux.conf
+sudo wget https://github.com/solomonxie/cdn/raw/master/server/tmux/tmux.conf -O ~/.tmux.conf

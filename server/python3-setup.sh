e1bd76ae7d729192b6cf9f75040a47838a7edab7
Solomon Xie <solomonxiewise@gmail.com>
2018-08-04 00:09:37 +0800

    Update from Solomon-mba.local

--- server/python3-setup.sh
+++ server/python3-setup.sh
@@ -7,5 +7,11 @@
 yes | sudo apt-get install python python3
 sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 yes | sudo python get-pip.py
+echo "========= Upgrading Python ==========="
 yes | sudo pip install --upgrade pip
+echo "======== Installing virtualenv ========"
 yes | sudo pip install virtualenv pipenv
+
+# ======= Make venvs =========
+echo "======= Settingup virtual environments ======="
+cd ~ && virtualenv -p python3 venv3
--- server/zsh/zsh-setup.sh
+++ server/zsh/zsh-setup.sh
@@ -11,6 +11,7 @@ sudo curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.
 echo "========== (INSTALLING PLUGINS FOR ZSH) ==========="
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+sudo wget https://raw.githubusercontent.com/agnoster/agnoster-zsh-theme/master/agnoster.zsh-theme -P ~/.oh-my-zsh/themes/
 sudo git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 yes | sudo pip install --user powerline-status
 # Setup configs

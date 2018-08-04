536b0d6cdd785ea24967fac808c0c93491dbe6f7
Solomon Xie <solomonxiewise@gmail.com>
2018-08-04 21:01:45 +0800

    Update from Solomon-mba.local

--- server/zsh/zsh-setup.sh
+++ server/zsh/zsh-setup.sh
@@ -15,8 +15,9 @@ sudo wget https://github.com/solomonxie/cdn/raw/master/server/zsh/zshrc.extensio
 echo "========== (INSTALLING OH-MY-ZSH) ==========="
 sudo curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 echo "========== (INSTALLING PLUGINS FOR ZSH) ==========="
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-sudo wget https://raw.githubusercontent.com/agnoster/agnoster-zsh-theme/master/agnoster.zsh-theme -P ~/.oh-my-zsh/themes/
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+echo "======= (Installing Themes for ZSH) ========="
 sudo git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+sudo wget https://raw.githubusercontent.com/agnoster/agnoster-zsh-theme/master/agnoster.zsh-theme -P ~/.oh-my-zsh/themes/
 yes | sudo pip install --user powerline-status

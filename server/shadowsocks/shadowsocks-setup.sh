8bd77d2fffcde6259525115afa9a75a69d150261
Solomon Xie <solomonxiewise@gmail.com>
2018-08-04 19:53:33 +0800

    improved shadowsocks script

--- server/shadowsocks/shadowsocks-setup.sh
+++ server/shadowsocks/shadowsocks-setup.sh
@@ -5,12 +5,15 @@
 #
 #
 # =======SETING UP SHADOWSOCKS =======
+
 # Install Shadowsocks
 echo "========== (INSTALLING SHADOWSOCKS) ==========="
 yes | sudo pip install shadowsocks
 if [$? != 0]; then
     exit 1;
 fi
+
+
 # Create config file
 echo "========== (OVERWRITING SHADOWSOCK'S CONFIG) ==========="
 sudo touch ~/shadowsocks.json
@@ -30,16 +33,16 @@ sudo cat> ~/shadowsocks.json <<EOF
     "fast_open":false
 }
 EOF
-# Auto start Shadowsocks service when system starts
-echo "========== (Rewrite SYSTEM START commands with Shadowsocks) ==========="
-sudo cat> /etc/rc.local <<EOF
-#!/bin/sh -e
-sudo ssserver -c /etc/shadowsocks.json -d start
-exit 0;
-EOF
+
 
 echo "========== (STARTING SHADOWSOCKS SERVER) ==========="
-sudo ssserver -c /etc/shadowsocks.json -d start
+sudo ssserver -c ~/shadowsocks.json -d start
 if [$? != 0];then
     echo "====Error occurred when starting the shadowsocks====="
 fi
+
+
+# Auto start Shadowsocks service when system starts
+echo "========== (Rewrite SYSTEM START commands with Shadowsocks) ==========="
+sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/rc.local -O /etc/rc.local
+

a95547b38a773d2901a6c1b431e92d65e5272ffe
Solomon Xie <solomonxiewise@gmail.com>
2018-08-04 19:34:16 +0800

    add error catch statements

---
+++ common-programs.sh
@@ -0,0 +1,10 @@
+# Owner: Solomon Xie
+# Email: solomonxiewise@gmail.com
+# Preload:
+# Commands:
+#
+
+echo "========== (INSTALLING OFTEN USED PROGRAMS) ==========="
+yes | sudo apt-get install htop glances
+yes | sudo pip install -U youtube-dl you-get
+yes | sudo pip install --user pipenv

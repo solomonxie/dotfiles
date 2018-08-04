a1767eed73f26b2af031112a892a19aaf9d9ce77
Solomon Xie <solomonxiewise@gmail.com>
2018-08-04 21:11:00 +0800

    Update from Solomon-mba.local

--- server/vim/vim-setup.sh
+++ server/vim/vim-setup.sh
@@ -4,19 +4,16 @@
 #
 #
 # =======SETING UP VIM =======
-# Download Vundle (Plugin Manager)
+echo "========== (OVERWRITING VIMRC CONFIG) ==========="
+sudo wget https://github.com/solomonxie/cdn/raw/master/server/vim/vimrc -O ~/.vimrc
 echo "========== (DOWNLOADING VUNDLE - VIM PLUGIN MANAGER) ==========="
 sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# Download color theme
 echo "========== (INSTALLING VIM COLOR SCHEME) ==========="
 sudo mkdir ~/.vim/colors
 sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/colors/gruvbox.vim -O ~/.vim/colors/gruvbox.vim
-# Download preset vimrc config file
-echo "========== (OVERWRITING VIMRC CONFIG) ==========="
-sudo wget https://github.com/solomonxie/cdn/raw/master/server/vim/vimrc -O ~/.vimrc
-# Upgrade vim
 echo "========== (UPGRADING VIM) ==========="
-yes | sudo apt-get upgrade vim
+# buggy one
+#yes | sudo apt-get upgrade vim
 if [$? != 0];then
     echo "=======Failed on upgrading vim======="
     exit 1;

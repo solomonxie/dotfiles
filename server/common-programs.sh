374528f20dab7a6b34d570664b7b9196242989f4
Solomon Xie <solomonxiewise@gmail.com>
2018-08-06 02:50:01 +0800

    Update from Solomon-mba.local

--- server/common-programs.sh
+++ server/common-programs.sh
@@ -5,5 +5,5 @@
 #
 
 echo "========== (INSTALLING OFTEN USED PROGRAMS) ==========="
-yes | sudo apt-get install htop glances speedtest-cli
+yes | sudo apt-get install mosh htop glances speedtest-cli
 yes | sudo pip install -U youtube-dl you-get tldr

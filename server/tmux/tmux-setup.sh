d32ee4bd2164d9f73399f919f6db728ff436794f
Solomon Xie <solomonxiewise@gmail.com>
2018-08-06 03:14:04 +0800

    Update from Solomon-mba.local

--- server/tmux/tmux-setup.sh
+++ server/tmux/tmux-setup.sh
@@ -12,6 +12,11 @@ fi
 echo "===== (Overwrite .tmux.conf) ======="
 sudo wget https://github.com/solomonxie/cdn/raw/master/server/tmux/tmux.conf -O ~/.tmux.conf
 echo "===== (Installing TPM for Tmux) ======="
-sudo mkdir ~/.tmux/resurrect
 sudo chown -R ubuntu ~/.tmux
 sudo git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "===== (Recover preset Tmux session) ======="
+sudo mkdir ~/.tmux/resurrect
+sudo wget https://github.com/solomonxie/cdn/raw/master/server/tmux/tmux_resurrect_last.txt -O ~/.tmux/resurrect/tmux_resurrect_last.txt
+sudo ln -sf ~/.tmux/resurrect/tmux_resurrect_last.txt ~/.tmux/resurrect/last
+
+

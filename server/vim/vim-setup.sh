30fa9fe744cf5487ed4ceffc5eb3d2dd67ab90d4
Solomon Xie <solomonxiewise@gmail.com>
2018-08-06 03:06:46 +0800

    Update from Solomon-mba.local

--- server/vim/vim-setup.sh
+++ server/vim/vim-setup.sh
@@ -6,11 +6,14 @@
 # =======SETING UP VIM =======
 echo "========== (OVERWRITING VIMRC CONFIG) ==========="
 sudo wget https://github.com/solomonxie/cdn/raw/master/server/vim/vimrc -O ~/.vimrc
+
+echo "========== (Change permission) ==========="
+sudo chown ubuntu ~/.vim
 echo "========== (DOWNLOADING VUNDLE - VIM PLUGIN MANAGER) ==========="
 sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "========== (INSTALLING VIM COLOR SCHEME) ==========="
 sudo mkdir ~/.vim/colors
-sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/colors/gruvbox.vim -O ~/.vim/colors/gruvbox.vim
+sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/vim/colors/gruvbox.vim -O ~/.vim/colors/gruvbox.vim
 echo "========== (UPGRADING VIM) ==========="
 # buggy one
 #yes | sudo apt-get upgrade vim

ff3586c1e0a433f0e6ed76f3601866508e77f5be
Solomon Xie <solomonxiewise@gmail.com>
2018-08-22 23:25:32 +0800

    Update from Solomon-mba.lan

--- Rpi/tmux/tmux-setup.sh
+++ Rpi/tmux/tmux-setup.sh
@@ -3,6 +3,8 @@
 # Enviroment: Raspberry Pi (Raspbian)
 # Preload:
 # Commands:
+#   1. Get in tmux
+#   2. Ctrl-b + Ctrl-I
 #
 # =======SETING UP TMUX =======
 sudo apt-get install tmux
@@ -10,14 +12,17 @@ if [$? != 0];then
     echo "=====Failed on installing Tmux======="
     exit 1;
 fi
+
 echo "===== (Overwrite .tmux.conf) ======="
 sudo wget https://github.com/solomonxie/cdn/raw/master/Rpi/tmux/tmux.conf -O ~/.tmux.conf
+
 echo "===== (Installing TPM for Tmux) ======="
+sudo mkdir ~/.tmux
 sudo chown -R pi ~/.tmux
 sudo git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo chown -R pi ~/.tmux/plugins
+
 echo "===== (Recover preset Tmux session) ======="
 sudo mkdir ~/.tmux/resurrect
 sudo wget https://github.com/solomonxie/cdn/raw/master/Rpi/tmux/resurrect/last.txt -O ~/.tmux/resurrect/last.txt
 sudo ln -sf ~/.tmux/resurrect/last.txt ~/.tmux/resurrect/last
-
-
--- Rpi/vim/vim-setup.sh
+++ Rpi/vim/vim-setup.sh
@@ -4,16 +4,23 @@
 #
 #
 # =======SETING UP VIM =======
+
+
 echo "========== (OVERWRITING VIMRC CONFIG) ==========="
-sudo wget https://github.com/solomonxie/cdn/raw/master/Rpi/vim/vimrc -O ~/.vimrc
+sudo touch ~/.vimrc
+sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/Rpi/vim/vimrc -O ~/.vimrc
 
 echo "========== (Change permission) ==========="
+sudo mkdir ~/.vim
 sudo chown pi ~/.vim
+
 echo "========== (DOWNLOADING VUNDLE - VIM PLUGIN MANAGER) ==========="
 sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 echo "========== (INSTALLING VIM COLOR SCHEME) ==========="
 sudo mkdir ~/.vim/colors
 sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/Rpi/vim/colors/gruvbox.vim -O ~/.vim/colors/gruvbox.vim
+
 echo "========== (UPGRADING VIM) ==========="
 # buggy one
 #yes | sudo apt-get upgrade vim

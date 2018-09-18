8f05ed0d57d5632fdd7642177adf03ef7ba97542
Solomon Xie <solomonxiewise@gmail.com>
2018-09-18 17:42:37 +0800

    add speedtest script

---
+++ server/speedtest.sh
@@ -0,0 +1,24 @@
+#! /bin/bash 
+#
+# Owner: Solomon Xie
+# Email: solomonxiewise@gmail.com
+# Notice:
+#     This script is run on local machine to test server speed.
+
+SERVERIP=$1
+
+#touch ~/log_vps_speed_test.txt
+#echo "\n========(Speed Test with IP)=========\n"
+
+# Speed of SSH connection
+time ssh ubuntu@$SERVERIP "exit"
+
+# Speed of uploading/download file with local machine
+time scp ~/scptest.jpg ubuntu@IP:~/
+time scp ubuntu@$SERVERIP:~/scptest.jpg ~/
+
+# Speed of connection to Youtube
+time ssh ubuntu@$SERVERIP "ping -c 1 youtube.com; exit"
+
+# Internet speed of remote server 
+time ssh ubuntu@$SERVERIP "speedtest; exit"

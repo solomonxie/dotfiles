dc39a8de2083466fc0dbea088c96b3c48bb318e9
Solomon Xie <solomonxiewise@gmail.com>
2018-09-18 16:51:25 +0800

    update ssserver

--- server/shadowsocks/shadowsocks-setup.sh
+++ server/shadowsocks/shadowsocks-setup.sh
@@ -1,7 +1,8 @@
 # Owner: Solomon Xie
 # Email: solomonxiewise@gmail.com
 # Enviroment: MacOS Sierra / Ubuntu
-#
+# How to run this script:
+#    $ sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/server/shadowsocks/shadowsocks-setup.sh | sh
 #
 #
 # =======SETING UP SHADOWSOCKS =======
@@ -16,14 +17,14 @@ fi
 
 # Create config file
 echo "========== (CREAT SHADOWSOCK'S CONFIG) ==========="
-sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/shadowsocks/shadowsocks.json -O ~/shadowsocks.json
+sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/shadowsocks/shadowsocks.json -O /etc/shadowsocks.json
 if [$? != 0]; then
     echo "======Failed to create shadowsocks.json====="
     exit 1;
 fi
 
 echo "========== (STARTING SHADOWSOCKS SERVER) ==========="
-sudo ssserver -c ~/shadowsocks.json -d start
+sudo ssserver -c /etc/shadowsocks.json -d start
 if [$? != 0];then
     echo "====Error occurred when starting the shadowsocks====="
 fi

20d565274bbe418f0705098f15a0eab79461a5a1
Solomon Xie <solomonxiewise@gmail.com>
2018-09-18 17:05:36 +0800

    Update from Solomon-mba.lan

--- server/init.sh
+++ server/init.sh
@@ -6,7 +6,7 @@
 # Notice: 
 #    This script should be run by "$ sudo bash xxx.sh"
 # How to run this script:
-#    $ sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/server/server-init.sh | sh
+#    $ sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/server/init.sh | sh
 
 
 # Update server & install essentials

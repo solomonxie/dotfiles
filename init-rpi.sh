007c0add258ba6df3546fc2a12b16f87e54d015d
Solomon Xie <solomonxiewise@gmail.com>
2018-09-26 18:38:54 +0800

    Update from Solomon-mba.lan

--- init-rpi.sh
+++ init-rpi.sh
@@ -9,7 +9,7 @@
 #    curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/init-rpi.sh | sudo sh
 
 # 
-echo "========== (Overwrite Raspberry Pi Default configs) ==========="
+echo "--------------------[   Overwrite Raspberry Pi Default configs   ]---------------------"
 sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/etc/Rpi/boot/config.txt -O /boot/config.txt
 # Enable ssh
 sudo touch /boot/ssh
@@ -18,45 +18,45 @@ sudo touch /boot/ssh
 
 
 # Update server & install essentials
-echo "========== (INITIAL UPDATE OF UBUNTU) ==========="
+echo "--------------------[   INITIAL UPDATE OF UBUNTU   ]---------------------"
 sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/etc/Rpi/sources.list -O /etc/apt/sources.list
 yes | sudo apt-get update
 
 # Setup Python3
-echo "========== (DOWNLOADING BASH SCRIPT FOR SETTING UP PYTHON3) ==========="
-sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/python/python3-setup-rpi.sh | sh
+echo "--------------------[   DOWNLOADING BASH SCRIPT FOR SETTING UP PYTHON3   ]---------------------"
+curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/python/python3-setup-rpi.sh | sudo sh
 
 # Setup ZSH
-echo "========== (DOWNLOADING BASH SCRIPT FOR SETTING UP ZSH) ==========="
-sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/zsh/zsh-setup-rpi.sh | sh
+echo "--------------------[   DOWNLOADING BASH SCRIPT FOR SETTING UP ZSH   ]---------------------"
+curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/zsh/zsh-setup-rpi.sh | sudo sh
 
 # Setup Vim
-echo "========== (DOWNLOADING BASH SCRIPT FOR SETTING UP VIM) ==========="
-sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/vim/vim-setup-rpi.sh | sh
+echo "--------------------[   DOWNLOADING BASH SCRIPT FOR SETTING UP VIM   ]---------------------"
+curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/vim/vim-setup-rpi.sh | sudo sh
 
 # Setup Tmux
-echo "========== (DOWNLOADING BASH SCRIPT FOR SETTING UP TMUX) ==========="
-sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/tmux/tmux-setup-rpi.sh | sh
+echo "--------------------[   DOWNLOADING BASH SCRIPT FOR SETTING UP TMUX   ]---------------------"
+curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/tmux/tmux-setup-rpi.sh | sudo sh
 
 
 # Setup Webdav
-#echo "========== (DOWNLOADING BASH SCRIPT FOR SETTING UP Webdav) ==========="
-#sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/webdav/webdav-setup-rpi.sh | sh
+#echo "--------------------[   DOWNLOADING BASH SCRIPT FOR SETTING UP Webdav   ]---------------------"
+#curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/webdav/webdav-setup-rpi.sh | sudo sh
 
 
 
 # Install Useful Programs
-echo "========== (DOWNLOADING BASH SCRIPT FOR Installing common programs) ==========="
-sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/etc/Rpi/common-programs-rpi.sh | sh
+echo "--------------------[   DOWNLOADING BASH SCRIPT FOR Installing common programs   ]---------------------"
+curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/etc/Rpi/common-programs-rpi.sh | sudo sh
 
 
 
 
 # [THIS SECTION SHOULD BETTER BE PLACED AT THE BOTTOM]
-echo "========== (SET UP CRONTAB JOBS) ==========="
-sudo curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/etc/Rpi/crontab.txt |crontab
+echo "--------------------[   SET UP CRONTAB JOBS   ]---------------------"
+curl -L https://raw.githubusercontent.com/solomonxie/cdn/master/etc/Rpi/crontab.txt |crontab
 
-echo "========== (Rewrite SYSTEM START commands with Shadowsocks) ==========="
+echo "--------------------[   Rewrite SYSTEM START commands with Shadowsocks   ]---------------------"
 sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/etc/Rpi/rc.local -O /etc/rc.local
 
 

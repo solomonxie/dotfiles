6247f5dbcefb5a3df13892812de6ca33daec4fdc
Solomon Xie <solomonxiewise@gmail.com>
2018-09-26 12:19:11 +0800

    removed expections catching

--- server/vim/vim-setup.sh
+++ server/vim/vim-setup.sh
@@ -5,21 +5,25 @@
 
 cd ~/.init
 
+mkdir ~/.vim
+export VIM=~/.vim
+
 # =======SETING UP VIM =======
 echo "========== (OVERWRITING VIMRC CONFIG) ==========="
-sudo wget https://github.com/solomonxie/cdn/raw/master/server/vim/vimrc -O ~/.vimrc >> log_vim.txt 2>&1
+wget https://github.com/solomonxie/cdn/raw/master/server/vim/vimrc -O ~/.vimrc >> log_vim.txt 2>&1
 
-echo "========== (Change permission) ==========="
-sudo chown ubuntu ~/.vim >> log_vim.txt 2>&1
+
 echo "========== (DOWNLOADING VUNDLE - VIM PLUGIN MANAGER) ==========="
-sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >> log_vim.txt 2>&1
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >> log_vim.txt 2>&1
+
+
+#echo "========== (Change permission) ==========="
+#sudo chown $USER $VIM >> log_vim.txt 2>&1
+
 echo "========== (INSTALLING VIM COLOR SCHEME) ==========="
-sudo mkdir ~/.vim/colors >> log_vim.txt 2>&1
-sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/vim/colors/gruvbox.vim -O ~/.vim/colors/gruvbox.vim >> log_vim.txt 2>&1
-echo "========== (UPGRADING VIM) ==========="
+mkdir ~/.vim/colors >> log_vim.txt 2>&1
+wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/vim/colors/gruvbox.vim -O ~/.vim/colors/gruvbox.vim >> log_vim.txt 2>&1
+
 # buggy one
+#echo "========== (UPGRADING VIM) ==========="
 #yes | sudo apt-get upgrade vim
-if [$? != 0];then
-    echo "=======Failed on upgrading vim======="
-    exit 1;
-fi

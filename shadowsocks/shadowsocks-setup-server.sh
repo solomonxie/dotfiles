e7b717d452fbcc52812c8c698433158311e63cf6
Solomon Xie <solomonxiewise@gmail.com>
2018-09-26 17:41:39 +0800

    reorganize folder structures

--- shadowsocks/shadowsocks-setup-server.sh
+++ shadowsocks/shadowsocks-setup-server.sh
@@ -15,7 +15,7 @@ yes | sudo pip install -U shadowsocks >> log_shadowsocks.txt
 
 # Create config file
 echo "-----[  CREAT SHADOWSOCK'S CONFIG   ]-----"
-sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/server/shadowsocks/shadowsocks.json -O /etc/shadowsocks.json  >> log_shadowsocks.txt
+sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/shadowsocks/ssserver.json -O /etc/ssserver.json  >> log_shadowsocks.txt
 
 echo "-----[  STARTING SHADOWSOCKS SERVER   ]-----"
-sudo ssserver -c /etc/shadowsocks.json -d start >> log_shadowsocks.txt
+sudo ssserver -c /etc/ssserver.json -d start >> log_shadowsocks.txt

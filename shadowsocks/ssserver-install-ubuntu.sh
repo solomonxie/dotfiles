a33633e963be42d57e0d4bf04873fc4aa2055063
Solomon Xie <solomonxiewise@gmail.com>
2018-10-01 14:04:30 +0800

    take scripts into functions

--- shadowsocks/ssserver-install-ubuntu.sh
+++ shadowsocks/ssserver-install-ubuntu.sh
@@ -8,13 +8,26 @@
 # ---SETING UP SHADOWSOCKS SERVER ---
 
 
-# Install Shadowsocks
-echo "-----[  INSTALLING SHADOWSOCKS   ]-----"
-yes | pip install -U shadowsocks >> ~/.init/log_shadowsocks.txt
 
-# Create config file
-echo "-----[  CREAT SHADOWSOCK'S CONFIG   ]-----"
-sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/shadowsocks/ssserver.json -O /etc/ssserver.json  >> ~/.init/log_shadowsocks.txt
+do_install_ssserver_manually(){
+    # Install Shadowsocks
+    echo "-----[  INSTALLING SHADOWSOCKS   ]-----"
+    yes | pip install -U shadowsocks >> ~/.init/log_shadowsocks.txt
 
-echo "-----[  STARTING SHADOWSOCKS SERVER   ]-----"
-sudo ssserver -c /etc/ssserver.json -d start >> ~/log_ssserver.txt
+    # Create config file
+    echo "-----[  CREAT SHADOWSOCK'S CONFIG   ]-----"
+    sudo wget https://raw.githubusercontent.com/solomonxie/cdn/master/shadowsocks/ssserver.json -O /etc/ssserver.json  >> ~/.init/log_shadowsocks.txt
+
+    echo "-----[  STARTING SHADOWSOCKS SERVER   ]-----"
+    sudo ssserver -c /etc/ssserver.json -d start >> ~/log_ssserver.txt
+}
+
+do_install_ssserver_docker(){
+    docker run -dt --name ssserver --restart always \
+    -p 6443:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" \
+    -s "-s 0.0.0.0 -p 6443 -m chacha20 -k shadow123 --fast-open" \
+    -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2 -key kcp123 -crypt aes-128"
+}
+
+do_install_ssserver_manually
+do_install_ssserver_docker
--- web-servers/jekyll.sh
+++ web-servers/jekyll.sh
@@ -4,23 +4,32 @@
 # Commands:
 #
 
-echo "========== (Installing Ruby & essentials) ==========="
-sudo apt-get install ruby ruby-dev build-essential
-echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
-echo 'export GEM_HOME=$HOME/gems' >> ~/.bashrc
-echo 'export PATH=$HOME/gems/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+
+do_install_jekyll_manually(){
+    echo "========== (Installing Ruby & essentials) ==========="
+    sudo apt-get install ruby ruby-dev build-essential
+    echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+    echo 'export GEM_HOME=$HOME/gems' >> ~/.bashrc
+    echo 'export PATH=$HOME/gems/bin:$PATH' >> ~/.bashrc
+    source ~/.bashrc
 
 
-echo "========== (updating gem) ==========="
-gem sources --add http://gems.github.com 
-gem sources --remove https://rubygems.org/
-gem sources --update
-yes | sudo gem update --system
+    echo "========== (updating gem) ==========="
+    gem sources --add http://gems.github.com 
+    gem sources --remove https://rubygems.org/
+    gem sources --update
+    yes | sudo gem update --system
 
-echo "========== (Installing Jekyll/Bundler) ==========="
-sudo gem install jekyll
-sudo gem install bundler
+    echo "========== (Installing Jekyll/Bundler) ==========="
+    sudo gem install jekyll
+    sudo gem install bundler
 
-echo "========== (Installing npm/gulp) ==========="
+    echo "========== (Installing npm/gulp) ==========="
+}
 
+
+do_install_jekyll_docker(){
+    echo ""
+}
+
+do_install_jekyll_manually

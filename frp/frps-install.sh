a51f00a53ea7ed0cddd59d6009ec638f61b1f561
Solomon Xie <solomonxiewise@gmail.com>
2018-10-25 00:04:44 +0800

    improv frp

--- frp/frps-install.sh
+++ frp/frps-install.sh
@@ -3,3 +3,19 @@
 # Preload:
 # Commands:
 #
+
+do_install_frp_server_ubuntu(){
+    cd $HOME
+    wget https://github.com/fatedier/frp/releases/download/v0.21.0/frp_0.21.0_linux_amd64.tar.gz
+    tar -xzvf frp*.tar.gz && mv frp_*/ frp && cd frp
+    curl -sSL https://raw.githubusercontent.com/solomonxie/cdn/master/frp/frps-ubuntu.ini -o frps.ini
+}
+
+run_frp_server_ubuntu(){
+    cd $HOME/frp/
+    ./frps -c ./frps.ini
+}
+
+run_frp_server_as_service_ubuntu(){
+    echo ""
+}

defac8873399be0516a04f32b218d0255aef1831
Solomon Xie <solomonxiewise@gmail.com>
2018-10-27 10:17:37 +0800

    Update from solomonxie

--- frp/install-frp-server.sh
+++ frp/install-frp-server.sh
@@ -4,18 +4,22 @@
 # Commands:
 #
 
-do_install_frp_server_ubuntu(){
-    cd $HOME
+DIR=$HOME
+
+do_install_frps_ubuntu(){
+    cd $DIR
     wget https://github.com/fatedier/frp/releases/download/v0.21.0/frp_0.21.0_linux_amd64.tar.gz
     tar -xzvf frp*.tar.gz && mv frp_*/ frp && cd frp
     curl -sSL https://raw.githubusercontent.com/solomonxie/cdn/master/frp/frps-ubuntu.ini -o frps.ini
 }
 
-run_frp_server_ubuntu(){
-    cd $HOME/frp/
+run_frps_ubuntu(){
+    cd $DIR/frp/
     ./frps -c ./frps.ini
 }
 
-run_frp_server_as_service_ubuntu(){
-    echo ""
+run_frps_as_service_ubuntu(){
+    sudo curl -sSL https://raw.githubusercontent.com/solomonxie/cdn/master/frp/frps.service -o /etc/systemd/system/frps.service
+    sudo systemctl enable frps
+    sudo systemctl status frps
 }

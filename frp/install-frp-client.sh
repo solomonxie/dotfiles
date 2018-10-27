df490b1f8c8cb7fe6490a8acb6ec8374a36043d2
Solomon Xie <solomonxiewise@gmail.com>
2018-10-27 20:20:08 +0800

    Update from solomonxie

--- frp/install-frp-client.sh
+++ frp/install-frp-client.sh
@@ -3,3 +3,25 @@
 # Preload:
 # Commands:
 #
+
+DIR=$HOME
+
+do_install_frpc_rpi(){
+    VER="0.21.0_linux_arm"
+    cd $DIR
+    wget https://github.com/fatedier/frp/releases/download/v0.21.0/frp_$VER.tar.gz
+    tar -xzvf frp_$VER.tar.gz
+    cd frp_$VER
+    curl -sSL https://raw.githubusercontent.com/solomonxie/cdn/master/frp/frpc-rpi.ini -o frpc.ini
+}
+
+run_frpc_rpi(){
+    cd $DIR
+    ./frpc -c ./frpc.ini
+}
+
+run_frpc_as_service_rpi(){
+    sudo curl -sSL https://raw.githubusercontent.com/solomonxie/cdn/master/frp/frpc.service -o /etc/systemd/system/frpc.service \
+    && sudo systemctl enable frpc
+    && sudo systemctl status frpc
+}

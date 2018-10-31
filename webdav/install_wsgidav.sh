4ca82855ff734526e0f4ed1a6a5bd9a6b9adc067
Solomon Xie <solomonxiewise@gmail.com>
2018-11-01 00:59:37 +0800

    add S3FS

--- webdav/install_wsgidav.sh
+++ webdav/install_wsgidav.sh
@@ -12,5 +12,21 @@ do_install_wsgidav(){
 }
 
 run_wsgidav(){
-    wsgidav --host=0.0.0.0 --port=8080 --root=~/webdav
+    # Without config file (current user, empty password)
+    wsgidav --no-config --host=0.0.0.0 --port=8080 --root=~/webdav
+    # With config file
+    wsgidav --config=my_config.yaml
+}
+
+install_s3fs_ubuntu(){
+    # https://cloud.netapp.com/blog/amazon-s3-as-a-file-system
+    sudo apt-get install -y s3fs
+    # Change key pairs to yours
+    echo ACCESS_KEY:SECRET_KEY > ~/.passwd-s3fs
+    chmod 600 ~/.passwd-s3fs
+    # Mount the aws S3 bucket
+    mkdir ~/s3-drive
+    /usr/bin/s3fs Your-Bucket-Name ~/s3-drive
+    # Show all the mounted devices
+    mount
 }

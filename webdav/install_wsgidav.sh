d68701574b57b56ac4aaf86e4a2ab38e61f40a15
Solomon Xie <solomonxiewise@gmail.com>
2018-11-05 14:32:49 +0800

    tested wsgidav config file and works well

--- webdav/install_wsgidav.sh
+++ webdav/install_wsgidav.sh
@@ -1,7 +1,6 @@
 #! /bin/bash 
 #
-# Owner: Solomon Xie
-# Email: solomonxiewise@gmail.com
+# MAINTAINER: Solomon Xie <solomonxiewise@gmail.com>
 # Enviroment: Python on any Unix-like OS
 # Description:
 #   WsgiDav is a Python implementation for Webadv Server
@@ -18,47 +17,3 @@ run_wsgidav(){
     wsgidav --config=my_config.yaml
 }
 
-install_s3fs_ubuntu(){
-    # https://cloud.netapp.com/blog/amazon-s3-as-a-file-system
-    # AWS S3 Auth info
-    BUCKET_NAME=''
-    ACCESS_KEY=''
-    SECRET_KEY=''
-    # Install
-    sudo apt-get install -y s3fs
-    # Change key pairs to yours
-    echo $ACCESS_KEY:$SECRET_KEY > ~/.passwd-s3fs
-    chmod 600 ~/.passwd-s3fs
-    # Mount the aws S3 bucket
-    mkdir ~/s3-drive
-    /usr/bin/s3fs $BUCKET_NAME ~/s3-drive -o umask=0007,uid=$(id -u),gid=$(id -g)
-    # Debugging mode
-    # /usr/bin/s3fs Your-Bucket-Name ~/s3-drive -o dbglevel=info -f -o curldbg
-    # Show all the mounted devices
-    mount
-    sudo ls ~/s3-drive
-}
-
-install_s3fs_mac(){
-    # AWS S3 Auth info
-    BUCKET_NAME=''
-    ACCESS_KEY=''
-    SECRET_KEY=''
-    # Install
-    brew cask install osxfuse
-    brew install s3fs
-    # Custom
-    # sudo /bin/cp -rfX /usr/local/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions
-    # sudo chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x
-    # Change key pairs to yours
-    echo $ACCESS_KEY:$SECRET_KEY > ~/.passwd-s3fs
-    chmod 600 ~/.passwd-s3fs
-    # Mount the aws S3 bucket
-    mkdir ~/s3-drive
-    /usr/bin/s3fs $BUCKET_NAME ~/s3-drive -o umask=0007,uid=$(id -u),gid=$(id -g)
-    # Debugging mode
-    # /usr/bin/s3fs Your-Bucket-Name ~/s3-drive -o dbglevel=info -f -o curldbg
-    # Show all the mounted devices
-    mount
-    sudo ls ~/s3-drive
-}

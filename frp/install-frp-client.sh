b6a7d6d10c46eb8af5c876df8fb681c99b019403
Solomon Xie <solomonxiewise@gmail.com>
2018-12-21 23:29:16 +0800

    fix frpc for multiple platforms

--- frp/install-frp-client.sh
+++ frp/install-frp-client.sh
@@ -1,30 +1,61 @@
-# Owner: Solomon Xie
-# Email: solomonxiewise@gmail.com
-# Preload:
-# Commands:
+#! /bin/bash 
 #
+# MAINTAINER: Solomon Xie <solomonxiewise@gmail.com>
+# Enviroment: Ubuntu / Rpi
+#
+# Run:
+#   $ 
+# Debug:
+#   $
 
-MYHOME=`getent passwd ${SUDO_UID:-$(id -u)} | cut -d: -f 6`
-REPO_URL="https://raw.githubusercontent.com/solomonxie/dotfiles/master"
+set -x
 
-VER="0.21.0_linux_arm"
-DIR=$MYHOME/$VER
+# Setup env variables and shared functions
+cd $(dirname $0); source "$(dirname $(pwd))/dotfiles.env"; cd -
 
-do_install_frpc_rpi(){
+#-------------------------------------
+#     Installation Methods
+#-------------------------------------
+
+do_install_frp_client(){
+    VER="0.21.0_linux_arm"
+    DIR=$MYHOME/$VER
+    # Do different things with different OS
+    case $MYOS in
+        ubuntu | raspbian)
+            do_install_frpc_dev
+            run_frpc_deb
+            run_frpc_as_service_deb
+            ;;
+        mac)
+            echo "Mac"
+            ;;
+    esac
+}
+
+
+do_install_frpc_deb(){
     cd ~
     wget https://github.com/fatedier/frp/releases/download/v0.21.0/frp_$VER.tar.gz
     tar -xzvf frp_$VER.tar.gz
     cd frp_$VER
-    wget $REPO_URL/frp/frpc-rpi.ini -O frpc.ini
+    cp $SRC/frp/frpc-rpi.ini ./frpc.ini
 }
 
-run_frpc_rpi(){
+run_frpc_deb(){
     cd $DIR
     ./frpc -c ./frpc.ini
 }
 
-run_frpc_as_service_rpi(){
-    sudo wget $REPO_URL/frp/frpc.service -O /etc/systemd/system/frpc.service \
-    && sudo systemctl enable frpc
-    && sudo systemctl status frpc
+run_frpc_as_service_deb(){
+    cp $SRC/frp/frpc.service /etc/systemd/system/frpc.service && \
+    sudo systemctl enable frpc && \
+    sudo systemctl status frpc
 }
+
+
+#-------------------------------------
+#          Entry points
+#-------------------------------------
+
+do_install_frp_client "$@"

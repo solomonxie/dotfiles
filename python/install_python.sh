2231745782ee06a8feb5a58f901afb9c07945bbf
Solomon Xie <solomonxiewise@gmail.com>
2019-01-14 01:46:58 +0800

    <fix> tmux mouse problem

--- python/install_python.sh
+++ python/install_python.sh
@@ -39,7 +39,11 @@ do_install_python3_deb(){
 }
 
 do_install_python3_mac(){
-    brew install python3 
+    brew install python3
+    # Install pip3 & pip3
+    curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+    sudo python2 /tmp/get-pip.py
+    sudo python3 /tmp/get-pip.py
 }
 
 

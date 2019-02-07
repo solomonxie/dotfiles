39733dda8da6e86170fe203635bbe295c9b373c3
Solomon Xie <solomonxiewise@gmail.com>
2019-02-07 14:20:52 +0800

    Update from solomonxie

--- shadowsocks/install_shadowsocks.sh
+++ shadowsocks/install_shadowsocks.sh
@@ -40,16 +40,16 @@ set_sslocal_service_pi(){
 
 build_ss_libev_pi(){
     # Dependencies
-    yes | sudo apt-get install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libc-ares-dev automake libmbedtls-dev libsodium-devA && \
+    sudo apt-get install --no-install-recommends -y gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libc-ares-dev automake libmbedtls-dev && \
     echo "[OK]"
-    mkdir -p ~/local/build_source/
-    cd ~/local/build_source/
+    mkdir -p /tmp/build_source/
+    cd /tmp/build_source/
     # Installation of libsodium
     export LIBSODIUM_VER=1.0.16
     wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz && \
     tar xvf libsodium-$LIBSODIUM_VER.tar.gz && \
     pushd libsodium-$LIBSODIUM_VER && \
-    ./configure --prefix=/opt && \
+    ./configure --prefix=/opt/libsodium && \
     make && sudo make install && \
     popd && sudo ldconfig && echo "[OK]"
     # Installation of MbedTLS
@@ -58,9 +58,9 @@ build_ss_libev_pi(){
     tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz && \
     pushd mbedtls-$MBEDTLS_VER && \
     make SHARED=1 CFLAGS=-fPIC && \
-    sudo make DESTDIR=/opt install && \
-    popd && sudo ldconfi && echo "[OK]"
-
+    sudo make DESTDIR=/opt/mbedtls install && \
+    popd && sudo ldconfig && echo "[OK]"
+    # Installation of Shadowsocks-libev
     git clone https://github.com/madeye/shadowsocks-libev.git
     cd shadowsocks-libev
     git submodule update --init --recursive

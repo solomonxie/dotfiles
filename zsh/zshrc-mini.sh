c7f81a1f77b6f7345a22b845e80111a6e35d5820
Solomon Xie <solomonxiewise@gmail.com>
2020-02-15 21:23:28 +0800

    stable vim version

--- zsh/zshrc-mini.sh
+++ zsh/zshrc-mini.sh
@@ -1,17 +1,4 @@
 # MAINTAINER: Solomon Xie <solomonxiewise@gmail.com>
-#
-# Preload dependencies:
-#     # Plugin: Syntax-highlighting
-#     $ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-#     # Plugin: zsh-autosuggestions
-#     $ git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-#     # Theme: Agnoster
-#     $ wget https://raw.githubusercontent.com/agnoster/agnoster-zsh-theme/master/agnoster.zsh-theme -P ~/.oh-my-zsh/themes/
-#     # Theme: Powerlevel9k
-#     $ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-#
-# Commands:
-#
 
 # User-specified hosts (not working at the moment)
     set HOSTALIASES=~/hosts
@@ -41,38 +28,13 @@
 # }
 
 # Builtin behaviors {
-    #zstyle ':completion:*' menu select # select completions with arrow keys
-    #zstyle ':completion:*' group-name '' # group results by category
-    # enable approximate matches for completion
-    #zstyle ':completion:::::' completer _expand _complete _ignored _approximate
-    # [ Conflict options ]
-        #setopt auto_list # automatically list choices on ambiguous completion
-        #setopt auto_menu # automatically use menu completion
-        #setopt always_to_end # move cursor to end if word had one match
-        #setopt correct_all # autocorrect commands
-    # [ Compatible with BASH ]
-        # -> Fixing error: "zsh: no matches found"
         setopt no_nomatch
-# }
-
-
-# Plugins {
-    # Fish-like commandline suggestions (not working with oh-my-zsh)
-    # source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-    # source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-    # Setups for plugins {
-        # [zsh-autosuggestions]
-        # ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=6' # [Essential]
-        # [zsh-history-substring-search]
-    # }
 # }
 
 
 # Key Bindings / Mappings {
     bindkey '^p' up-line-or-search # Up arrow
     bindkey '^n' down-line-or-search # Down arrow
-    # [autosuggestions] (history conflict with zsh hints, not recommanded)
-    #bindkey '^i' autosuggest-accecpt # [Essential] Ctrl+i to confirm hint
 # }
 
 

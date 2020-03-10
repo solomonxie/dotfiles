c03671abf82ede2fdc624b178c941f543d47a421
Solomon Xie <solomonxiewise@gmail.com>
2020-03-10 21:53:16 +0800

    solomonxie updated on branch [master]

---
+++ zsh/bashrc.sh
@@ -0,0 +1,13 @@
+#######################################################################
+#                                BASH                                 #
+#######################################################################
+
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/findutils/libexec/gnuman:$MANPATH"
+export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/gnu-tar/libexec/gnuman:$MANPATH"
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+

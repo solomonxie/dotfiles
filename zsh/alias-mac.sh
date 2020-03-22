c7bb3db345809be7ccd268c9c472fef8898f8091
Solomon Xie <solomonxiewise@gmail.com>
2020-03-22 20:02:02 +0800

    rearrange alias

--- zsh/alias-mac.sh
+++ zsh/alias-mac.sh
@@ -1,22 +1,33 @@
-# MAINTAINER: Solomon Xie <solomonxiewise@gmail>
-#
-# Description: Additional configs for zshrc, which aren't good for public.
-#
-#
-#
-# ==== ALIAS ====
+##################################################
+#                 _    _ _                       #
+#                / \  | (_) __ _ ___             #
+#               / _ \ | | |/ _` / __|            #
+#              / ___ \| | | (_| \__ \            #
+#             /_/   \_\_|_|\__,_|___/            #
+#                                                #
+##################################################
+# MAINTAINER: Solomon Xie <solomonxiewise@gmail> #
+##################################################
 
-# Builtin Utils
+#######################################################################
+#                            FOR BUILT-IN                             #
+#######################################################################
 alias lah="ls -lah"
 alias please="sudo"
 
-# Program replacements
+
+#######################################################################
+#                                 Vim                                 #
+#######################################################################
 alias vim="nvim"
 alias vi="vi --noplugin"
 alias rec="asciinema rec"
 alias m="macdown"
 
-# RCs, Config files
+
+#######################################################################
+#                             Bulk Edits                              #
+#######################################################################
 alias zshrc="vim -S ~/dotfiles/vim/sessions/zsh.vim"
 # alias zshrc0="vim ~/.zshrc"
 # alias zshrc2="vim ~/dotfiles/zsh/alias-mac.sh"
@@ -40,7 +51,10 @@ alias tigrc="vim ~/.tigrc"
 # alias vimchanges="vim $(git status --porcelain | awk '{print $2}')"
 alias snippets="vim -S ~/dotfiles/vim/sessions/snippets.vim "
 
-# Shadowsocks proxy
+
+#######################################################################
+#                               NETWORK                               #
+#######################################################################
 alias proxy='export all_proxy=http://127.0.0.1:1060'
 alias unproxy='unset all_proxy'
 alias myip="curl https://ip.cn"
@@ -50,10 +64,6 @@ function sslo() {
     nohup ss-local -c $1 -v 1> /tmp/ssclient.log 2> /tmp/ssclient-err.log &
 }
 
-# frequent commands
-alias ju="jupyter notebook"
-alias ipy="ipython"
-alias repos="cd ~/workspace/repos"
 #alias you="youtube-dl -civw --proxy "socks5://127.0.0.1:1086" -f best/mp4 --write-sub "
 alias you="youtube-dl"
 alias yourc="vim ~/.config/youtube-dl/config"
@@ -61,7 +71,10 @@ alias yourc="vim ~/.config/youtube-dl/config"
 #alias vfzf="vim $(fzf)"
 #alias dfzf="cd $(sh 'find * -type d | fzf')"
 
-# Git commands
+
+#######################################################################
+#                                 GIT                                 #
+#######################################################################
 alias gs="git status"
 alias gc="git add .; git commit"
 # alias gd="git diff --word-diff=plain"
@@ -72,35 +85,47 @@ alias gcp="git add .; git quickcommit; git pub"
 alias gcl="git clone"
 alias diff=vimdiff
 
-# Docker
+
+#######################################################################
+#                               DOCKER                                #
+#######################################################################
 alias gitbook='docker run -it --name gitbook --rm -v "$PWD":/gitbook -p 4000:4000 billryan/gitbook gitbook'
 
-# Tmux
-#alias tmux="TERM=screen-256color-bce tmux"
-alias pname="printf '\033]2;%s\033\\'"  # Set Tmux Pane title
-# Please do not Overwite the `title` command/function
 
-# Python Virtualenv
+#######################################################################
+#                               PYTHON                                #
+#######################################################################
 alias venv2="source ~/virtualenv/venv2/bin/activate"
 alias venv3="source ~/virtualenv/venv3/bin/activate"
 alias venv-ju="source ~/virtualenv/venv-ju/bin/activate"
 alias venv-nvim="source ~/virtualenv/venv-ju/bin/activate"
+alias ju="jupyter notebook"
+alias ipy="ipython"
 
-# IDE
+#######################################################################
+#                          MAC APPLICATIONS                           #
+#######################################################################
 # alias sub="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 # alias vsc="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
 alias sub="open -a 'Sublime Text'"
 alias code="open -a 'Visual Studio Code'"
 alias typora="open -a 'Typora'"
+alias chrome="open -a Google\ Chrome"
+alias safari="open -a Safari"
+# alias brew="HOMEBREW_NO_AUTO_UPDATE=1 brew"
 
-# Browsers
-alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-alias safari="/Applications/Safari.app/Contents/MacOS/Safari"
-alias opera="/Applications/Opera.app/Contents/MacOS/Opera"
 
-# Clients
+#######################################################################
+#                         CLI TOOLS/COMMANDS                          #
+#######################################################################
+# PostgreSQL clients
 if [ -x $(command -v pgcli) ]; then
     alias psql="$(command -v pgcli)"
 elif [ -x /Applications/Postgres.app/Contents/Versions/latest/bin/psql ];then
     alias psql="/Applications/Postgres.app/Contents/Versions/latest/bin/psql"
 fi
+
+# Tmux
+#alias tmux="TERM=screen-256color-bce tmux"
+alias pname="printf '\033]2;%s\033\\'"  # Set Tmux Pane title
+# Please do not Overwite the `title` command/function

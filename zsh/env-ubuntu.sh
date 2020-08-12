44c38bb84398e2d81e1e11f3810371045a99bed7
Solomon Xie <solomonxiewise@gmail.com>
2020-08-12 20:31:31 +0800

    xiaoboxie updated on branch [master]

--- zsh/env-ubuntu.sh
+++ zsh/env-ubuntu.sh
@@ -8,11 +8,14 @@ export LC_ALL=C
 # etc., cat 'expot path=...' > ~/.zshrc
 
 export PATH="/usr/local/sbin:$PATH"
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-export EDITOR=vim
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
-export OCTAVE_EXECUTABLE=/usr/local/bin/octave
-
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+# Python
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/virtualenv/venv2/bin:$PATH"
+export PATH="$HOME/virtualenv/venv/bin:$PATH"
+
+export EDITOR=vim
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+export OCTAVE_EXECUTABLE=/usr/local/bin/octave

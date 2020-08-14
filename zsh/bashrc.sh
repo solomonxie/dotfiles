fcbfaa064336ac9125ffb37c7da49648b909e2da
Solomon Xie <solomonxiewise@gmail.com>
2020-08-14 20:08:26 +0800

    xiaoboxie updated on branch [master]

--- zsh/bashrc.sh
+++ zsh/bashrc.sh
@@ -7,9 +7,23 @@ source ~/dotfiles/zsh/bash-alias.sh
 #                        ENVIRONMENT VARIABLES                        #
 #######################################################################
 # IMPORTANT!!
-export LC_ALL=en_US.UTF-8
+
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_COLLATE=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_PAPER=en_US.UTF-8
+export LC_NAME=en_US.UTF-8
+export LC_ADDRESS=en_US.UTF-8
+export LC_TELEPHONE=en_US.UTF-8
+export LC_MEASUREMENT=en_US.UTF-8
+export LC_IDENTIFICATION=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 
 export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"

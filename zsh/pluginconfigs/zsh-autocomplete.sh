c5aaa62d2c653c9fecd39cb84b7154de9be63293
Solomon Xie <solomonxiewise@gmail.com>
2020-08-17 14:47:06 +0800

    xiaoboxie updated on branch [master]

--- zsh/pluginconfigs/zsh-autocomplete.sh
+++ zsh/pluginconfigs/zsh-autocomplete.sh
@@ -4,3 +4,5 @@ if [ ! -e ~/.zsh/plugins/zsh-autocomplete ];then
 fi
 source ~/.zsh/plugins/zsh-autocomplete/zsh-autocomplete.zsh
 zstyle ':autocomplete:*:no-matches-yet' message ''  # Disable idle message
+zstyle ':autocomplete:space:*' magic correct-word expand-history
+zstyle ':autocomplete:tab:*' completion cycle

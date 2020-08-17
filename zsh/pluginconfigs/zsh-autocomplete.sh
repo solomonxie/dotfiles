04f2fb1f1bd1fa2dd02b6d07a29d8d54e2f84e13
Solomon Xie <solomonxiewise@gmail.com>
2020-08-17 11:26:37 +0800

    xiaoboxie updated on branch [master]

--- zsh/pluginconfigs/zsh-autocomplete.sh
+++ zsh/pluginconfigs/zsh-autocomplete.sh
@@ -1,4 +1,6 @@
+# https://github.com/marlonrichert/zsh-autocomplete
 if [ ! -e ~/.zsh/plugins/zsh-autocomplete ];then
     git clone https://github.com/marlonrichert/zsh-autocomplete.git ~/.zsh/plugins/zsh-autocomplete
 fi
 source ~/.zsh/plugins/zsh-autocomplete/zsh-autocomplete.zsh
+zstyle ':autocomplete:*:no-matches-yet' message ''  # Disable idle message

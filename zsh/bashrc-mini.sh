8ea0d62608499c6c53221e0d222a6dbae6bb0f93
Solomon Xie <solomonxiewise@gmail.com>
2021-05-25 21:35:50 +0800

    xiaoboxie updated on branch [master]

---
+++ zsh/bashrc-mini.sh
@@ -0,0 +1,129 @@
+#######################################################################
+#                        ENVIRONMENT VARIABLES                        #
+#######################################################################
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_COLLATE=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_PAPER=en_US.UTF-8
+export LC_NAME=en_US.UTF-8
+export LC_ADDRESS=en_US.UTF-8
+export LC_TELEPHONE=en_US.UTF-8
+export LC_MEASUREMENT=en_US.UTF-8
+export LC_IDENTIFICATION=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+
+export PATH="/usr/local/sbin:$PATH"
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+# Python
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/virtualenv/venv2/bin:$PATH"
+export PATH="$HOME/virtualenv/venv/bin:$PATH"
+
+export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+export LS_COLORS='no=00:fi=00:di=34:ow=34;40:ln=35:pi=30;44:so=35;44:do=35;44:bd=33;44:cd=37;44:or=05;37;41:mi=05;37;41:ex=01;31:*.cmd=01;31:*.exe=01;31:*.com=01;31:*.bat=01;31:*.reg=01;31:*.app=01;31:*.txt=32:*.org=32:*.md=32:*.mkd=32:*.h=32:*.c=32:*.C=32:*.cc=32:*.cpp=32:*.cxx=32:*.objc=32:*.sh=32:*.csh=32:*.zsh=32:*.el=32:*.vim=32:*.java=32:*.pl=32:*.pm=32:*.py=32:*.rb=32:*.hs=32:*.php=32:*.htm=32:*.html=32:*.shtml=32:*.erb=32:*.haml=32:*.xml=32:*.rdf=32:*.css=32:*.sass=32:*.scss=32:*.less=32:*.js=32:*.coffee=32:*.man=32:*.0=32:*.1=32:*.2=32:*.3=32:*.4=32:*.5=32:*.6=32:*.7=32:*.8=32:*.9=32:*.l=32:*.n=32:*.p=32:*.pod=32:*.tex=32:*.go=32:*.bmp=33:*.cgm=33:*.dl=33:*.dvi=33:*.emf=33:*.eps=33:*.gif=33:*.jpeg=33:*.jpg=33:*.JPG=33:*.mng=33:*.pbm=33:*.pcx=33:*.pdf=33:*.pgm=33:*.png=33:*.PNG=33:*.ppm=33:*.pps=33:*.ppsx=33:*.ps=33:*.svg=33:*.svgz=33:*.tga=33:*.tif=33:*.tiff=33:*.xbm=33:*.xcf=33:*.xpm=33:*.xwd=33:*.xwd=33:*.yuv=33:*.aac=33:*.au=33:*.flac=33:*.m4a=33:*.mid=33:*.midi=33:*.mka=33:*.mp3=33:*.mpa=33:*.mpeg=33:*.mpg=33:*.ogg=33:*.ra=33:*.wav=33:*.anx=33:*.asf=33:*.avi=33:*.axv=33:*.flc=33:*.fli=33:*.flv=33:*.gl=33:*.m2v=33:*.m4v=33:*.mkv=33:*.mov=33:*.MOV=33:*.mp4=33:*.mp4v=33:*.mpeg=33:*.mpg=33:*.nuv=33:*.ogm=33:*.ogv=33:*.ogx=33:*.qt=33:*.rm=33:*.rmvb=33:*.swf=33:*.vob=33:*.webm=33:*.wmv=33:*.doc=31:*.docx=31:*.rtf=31:*.dot=31:*.dotx=31:*.xls=31:*.xlsx=31:*.ppt=31:*.pptx=31:*.fla=31:*.psd=31:*.7z=1;35:*.apk=1;35:*.arj=1;35:*.bin=1;35:*.bz=1;35:*.bz2=1;35:*.cab=1;35:*.deb=1;35:*.dmg=1;35:*.gem=1;35:*.gz=1;35:*.iso=1;35:*.jar=1;35:*.msi=1;35:*.rar=1;35:*.rpm=1;35:*.tar=1;35:*.tbz=1;35:*.tbz2=1;35:*.tgz=1;35:*.tx=1;35:*.war=1;35:*.xpi=1;35:*.xz=1;35:*.z=1;35:*.Z=1;35:*.zip=1;35:*.ANSI-30-black=30:*.ANSI-01;30-brblack=01;30:*.ANSI-31-red=31:*.ANSI-01;31-brred=01;31:*.ANSI-32-green=32:*.ANSI-01;32-brgreen=01;32:*.ANSI-33-yellow=33:*.ANSI-01;33-bryellow=01;33:*.ANSI-34-blue=34:*.ANSI-01;34-brblue=01;34:*.ANSI-35-magenta=35:*.ANSI-01;35-brmagenta=01;35:*.ANSI-36-cyan=36:*.ANSI-01;36-brcyan=01;36:*.ANSI-37-white=37:*.ANSI-01;37-brwhite=01;37:*.log=01;32:*~=01;32:*#=01;32:*.bak=01;33:*.BAK=01;33:*.old=01;33:*.OLD=01;33:*.org_archive=01;33:*.off=01;33:*.OFF=01;33:*.dist=01;33:*.DIST=01;33:*.orig=01;33:*.ORIG=01;33:*.swp=01;33:*.swo=01;33:*,v=01;33:*.gpg=34:*.gpg=34:*.pgp=34:*.asc=34:*.3des=34:*.aes=34:*.enc=34:*.sqlite=34:'
+
+#######################################################################
+#                            FOR BUILT-IN                             #
+#######################################################################
+alias l='ls -l'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
+alias la='ls -A'
+alias lsa='ls -harl'
+alias lsd='ls -lah -d */'
+alias lah="ls -lah"
+alias please="sudo"
+
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias mv='mv -i'
+alias cp='cp -i'
+alias rm='rm -I --preserve-root'
+
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+
+
+#######################################################################
+#                                 Vim                                 #
+#######################################################################
+# [[ -e $(command -v nvim) ]] && alias vim="nvim" && alias vimdiff="nvim -d"
+alias vi="vi --noplugin"
+
+
+#######################################################################
+#                           CONFIG PROJECTS                           #
+#######################################################################
+alias zshrc="vim -S ~/dotfiles/vim/sessions/zsh.vim"
+# alias zshrc0="vim ~/.zshrc"
+# alias zshrc2="vim ~/dotfiles/zsh/alias-mac.sh"
+# alias zshrc3="vim ~/dotfiles/zsh/themes.sh"
+# alias vimrc="vim ~/dotfiles/vim/vimrc.vim"
+alias vimrc="vim -S ~/dotfiles/vim/sessions/vimrc_session.vim"
+#alias vimrc2="vim ~/dotfiles/vim/vimrc-plugins"
+#alias vimrc3="vim ~/dotfiles/vim/vimrc-keymappings"
+#alias vimrc4="vim ~/dotfiles/vim/vimrc-ui"
+#alias nvimrc="vim ~/dotfiles/vim/init.vim"
+# alias nvimrc="vim -S ~/dotfiles/vim/sessions/vimrc.vim"
+alias tmuxrc="vim ~/dotfiles/tmux/tmux.conf"
+alias bashrc="vim ~/.bashrc"
+alias rangerrc="vim ~/.config/ranger/rc.conf"
+#alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+alias muttrc="vim -S ~/dotfiles/vim/sessions/muttrc.vim"
+alias tigrc="vim ~/.tigrc"
+# alias vimchanges="vim $(git status --porcelain | awk '{print $2}')"
+alias snippets="vim -S ~/dotfiles/vim/sessions/snippets.vim "
+
+
+#######################################################################
+#                               NETWORK                               #
+#######################################################################
+alias proxy='export all_proxy=http://127.0.0.1:1061'
+alias unproxy='unset all_proxy'
+alias myip="curl https://httpbin.org/ip"
+alias hosts='sudo vim /etc/hosts'
+
+#alias you="youtube-dl -civw --proxy "socks5://127.0.0.1:1086" -f best/mp4 --write-sub "
+alias you="youtube-dl"
+alias yourc="vim ~/.config/youtube-dl/config"
+#alias fzf="~/.fzf/bin/fzf --height 40%"
+#alias vfzf="vim $(fzf)"
+#alias dfzf="cd $(sh 'find * -type d | fzf')"
+
+alias wgetwholesite="wget -r -p -np -k"
+alias wgetkeep="wget -x"
+
+
+#######################################################################
+#                                 GIT                                 #
+#######################################################################
+alias gs="git status"
+alias gc="git commit"
+alias gac="git add . && git commit"
+# alias gd="git diff --word-diff=plain"
+alias gd="git diff"
+alias gdh="git diff"
+alias gsh="git stash"
+alias gco="git checkout"
+alias gsc="git sync"
+alias gcp="git add .; git quickcommit; git pub"
+alias gcl="git clone"
+alias diff=vimdiff
+
+alias gitrc0="vim ~/.gitconfig"
+alias gitrc="vim ~/dotfiles/etc/git/gitconfig.ini"
+alias gitrc_="vim ./.git/config"

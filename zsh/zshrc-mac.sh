0e591be6ef5c3b6ad1181e033cc586c53d853d5a
Solomon Xie <solomonxiewise@gmail.com>
2021-08-26 17:59:54 +0800

    xiaoboxie updated on branch [master]

--- zsh/zshrc-mac.sh
+++ zsh/zshrc-mac.sh
@@ -151,6 +151,9 @@ export LC_IDENTIFICATION=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/nvim-osx64/bin:$PATH"
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/opt/homebrew/bin/:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/opt/curl/bin:$PATH"  # Curl newer version

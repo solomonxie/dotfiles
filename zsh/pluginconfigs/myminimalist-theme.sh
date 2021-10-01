3be1c69f5f13da3113ed4ea644932ad3c0b30076
Solomon Xie <solomonxiewise@gmail.com>
2021-10-01 17:25:48 +0800

    xiaoboxie updated on branch [master]

--- zsh/pluginconfigs/myminimalist-theme.sh
+++ zsh/pluginconfigs/myminimalist-theme.sh
@@ -6,6 +6,9 @@
 # {2} -> green
 # {51} -> blue
 #################################################################################
+autoload -Uz add-zsh-hook vcs_info
+setopt prompt_subst
+add-zsh-hook precmd vcs_info
 
 # export PROMPT='%n%F:%F{51}%~ $%f '  # bash like
 # export PROMPT='%F{51}$%f '  # Folder only
@@ -16,7 +19,14 @@
 export GIT_ROOT=`git rev-parse --show-toplevel ||true`
 export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD ||true`
 git_info="git:(${GIT_BRANCH})"
-export PROMPT="%F{51}% %. %F{10}% ${git_info} "
+# export PROMPT="%F{51}% %. %F{10}% ${git_info} "
+export PROMPT='%1~ %F{red}${vcs_info_msg_0_}%f $ '
+
+# zstyle ':vcs_info:*' check-for-changes true
+# zstyle ':vcs_info:*' unstagedstr ' *'
+# zstyle ':vcs_info:*' stagedstr ' +'
+# zstyle ':vcs_info:git:*' formats       '(%b%u%c)'
+# zstyle ':vcs_info:git:*' actionformats '(%b|%a%u%c)'
 
 # export PS1="%1~ $ "
 # export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '

421f8ed6fa3f1b7d35d94763b899e279bfbfb028
Solomon Xie <solomonxiewise@gmail.com>
2022-01-30 11:57:23 +0800

    install tz, duf

--- zsh/zshrc-mac.zsh
+++ zsh/zshrc-mac.zsh
@@ -81,6 +81,8 @@ setopt incappendhistory
 # export LSCOLORS='BXfxcxdxbxegedabagacad'  # BSD
 export LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'  # LINUX
 
+# REF: https://github.com/oz/tz
+export TZ_LIST="Asia/Shanghai,Beijing-Office;US/Pacific, US-Office"
 
 #######################################################################
 #                    PATH / ENVIRONMENT VARIABLES                     #
